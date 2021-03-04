3bff69f92ceef6c14b7abd4d247945afb85f1a0b
Dmitrii Evdokimov <diev@mail.ru>
2021-03-04 14:08:28 +0300

    Fix release

--- Program.cs
+++ Program.cs
@@ -31,7 +31,8 @@ namespace ICRS_NBKI_Request
 
             string uriPath = settings["Uri"] ?? "https://icrs.nbki.ru/products/B2BRequestServlet";
             string srcPath = settings["Requests"] ?? ".";
-            string dstPath = settings["Results"] ?? ".";
+            string bakPath = settings["RequestsBAK"] ?? "REQ";
+            string dstPath = settings["Results"] ?? "XML";
 
             // Use TLS 1.2 (required!)
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -39,32 +40,52 @@ namespace ICRS_NBKI_Request
             // Ignore any Cert validation or install the root ones from http://cpca.cryptopro.ru/cacer.p7b
             //ServicePointManager.ServerCertificateValidationCallback += (se, cert, chain, sslerror) => { return true; };
 
+            if (!Directory.Exists(bakPath))
+            {
+                Directory.CreateDirectory(bakPath);
+            }
+
+            if (!Directory.Exists(dstPath))
+            {
+                Directory.CreateDirectory(dstPath);
+            }
+
             var dir = new DirectoryInfo(srcPath);
             foreach (var file in dir.GetFiles("*.req"))
             {
                 string srcFile = file.FullName;
+                string bakFile = Path.Combine(bakPath, file.Name);
                 string dstFile = Path.Combine(dstPath, Path.ChangeExtension(file.Name, ".xml"));
 
-                DownloadFile(uriPath, srcFile, dstFile);
+                DownloadFile(uriPath, srcFile, dstFile, bakFile);
             }
 
             Environment.Exit(0);
         }
 
-        private static void DownloadFile(string uri, string src, string dst)
+        private static void DownloadFile(string uri, string src, string dst, string bak)
         {
             var client = new WebClient();
-            byte[] response = client.UploadFile(uri, src);
+            byte[] response = client.UploadData(uri, File.ReadAllBytes(src));
 
             if (response != null && response.Length > 0)
             {
+                //File.WriteAllBytes(dst + ".p7s", response);
+
                 // Clean XML from a PKCS#7 signature
                 var signedCms = new SignedCms();
                 signedCms.Decode(response);
-                byte[] data = signedCms.ContentInfo.Content;
+                response = signedCms.ContentInfo.Content;
 
-                File.WriteAllBytes(dst, data);
+                File.WriteAllBytes(dst, response);
                 Console.WriteLine($"File {dst} ready.");
+
+                if (File.Exists(bak))
+                {
+                    File.Delete(bak);
+                }
+
+                File.Move(src, bak);
             }
         }
     }
